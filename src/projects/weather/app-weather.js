240ea4d37a6beb0b52c7b2f51242e45688929b92
AnnVynnychenkoa <avinnichenko93@gmail.com>
2026-08-11 17:26:50 +0200

    feat(weather): create .env file and search function with forecast days

--- src/projects/weather/app-weather.js
+++ src/projects/weather/app-weather.js
@@ -1,11 +1,41 @@
-const BASE_URL = 'http://api.weatherapi.com/v1/';
-const API_KEY = '478ea07d54aa4674858141258260908&q';
+const BASE_URL = import.meta.env.VITE_WEATHER_BASE_URL;
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-async function getWeather() {
-  fetch(`${BASE_URL}forecast.json?key=${API_KEY}=Paris&days=2`)
-    .then(response => response.json())
-    .then(weather => console.log(weather))
-    .catch(err => console.log(err));
+const searchForm = document.querySelector('.js-search-form');
+const forecastDaysContainer = document.querySelector(
+  '.js-forecast-days-container'
+);
+
+function createMarkupForecastDays() {
+  let markup = '';
+  for (let i = 1; i <= 14; i += 1) {
+    markup += `<option data-id="${i}">${i}</option>`;
+  }
+  forecastDaysContainer.innerHTML = markup;
 }
 
-getWeather();
+createMarkupForecastDays();
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const enteredCity = event.target.elements.cityName.value.trim().toLowerCase();
+  const enteredDays = event.target.elements.forecastDays.value.trim();
+  getWeather(enteredCity, enteredDays);
+}
+
+async function getWeather(enteredCity, enteredDays) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/forecast.json?key=${API_KEY}&q=${enteredCity}&days=${enteredDays}`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+searchForm.addEventListener('submit', handleSubmit);
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
-
+  envDir: '../',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
